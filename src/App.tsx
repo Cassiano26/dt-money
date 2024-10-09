97ee8685005d5f696c69b069cf5945839494aee1
Cassiano26 <candidocassiano2@gmail.com>
2024-10-09 19:19:34 -0300

    Definindo Estilos globais e default theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
 
   return (
-    <h1>DT Money</h1>
+    <ThemeProvider theme={defaultTheme}>
+          <h1>DT Money</h1>
+    </ThemeProvider>
   )
 }
 
