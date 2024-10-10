89ea1cdf4853849eac1df0059e725f03c00c4cec
Cassiano26 <candidocassiano2@gmail.com>
2024-10-10 10:34:51 -0300

    Inserindo Font Roboto do google fonts e aplicando estilos globais.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-          <h1>DT Money</h1>
+      <GlobalStyle />
+      <h1>DT Money</h1>
     </ThemeProvider>
   )
 }
