411860d3834eb140c23635ac4a5d180e6ba9acd3
Cassiano26 <candidocassiano2@gmail.com>
2024-10-10 15:44:46 -0300

    Criando modal das novas transações.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/Logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransationModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransationModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
